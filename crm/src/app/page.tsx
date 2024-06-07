a5c0e1c336429edd8ca705f6e15684d10af3786c
KandaurovF <infinityride86@gmail.com>
2024-06-07 23:29:48 +0300

    Install tailwind css

--- crm/src/app/page.tsx
+++ crm/src/app/page.tsx
@@ -4,7 +4,7 @@ import NotActiveLabel from './components/notActive-label';
 export default function Home() {
   return (
     <main>
-      <h1>Home page</h1>
+      <h1 className="text-xl">Home page</h1>
       <ActiveLabel>Active lable</ActiveLabel>
       <NotActiveLabel>Not active lable</NotActiveLabel>
     </main>
