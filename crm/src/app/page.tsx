cb12d736d1dc1fa1b398888a6837cf9dec259eb0
KandaurovF <infinityride86@gmail.com>
2024-06-10 19:37:48 +0300

    add CompanyFormModal

--- crm/src/app/page.tsx
+++ crm/src/app/page.tsx
@@ -1,16 +1,19 @@
+import AddCompanyButton from './components/AddCompanyButton';
 import StatusLabel, { Status } from './components/status-label';
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      {/* <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
       <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
       <StatusLabel status={Status.Suspended} disabled>
         Suspended (Disabled)
-      </StatusLabel>
+      </StatusLabel> */}
+
+      <AddCompanyButton />
     </main>
   );
 }
