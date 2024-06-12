9f2e9740204b9c9ef37d6951cee44509b05d503b
KandaurovF <infinityride86@gmail.com>
2024-06-12 18:45:56 +0300

    add link and hooks for navigation

--- crm/src/app/components/Sidebar.tsx
+++ crm/src/app/components/Sidebar.tsx
@@ -1,8 +1,18 @@
+'use client';
+
 import React from 'react';
 import Image from 'next/image';
 import SidebarItems from './SidebarItems';
+import { usePathname, useRouter } from 'next/navigation';
 
 export default function Sidebar() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const hendleExitClick = () => {
+    router.push('/');
+  };
+
   return (
     <aside className="fixed top-0 left-0 z-50 w-60 h-screen">
       <div className="flex flex-col h-full overflow-y-auto bg-gray-900">
@@ -15,6 +25,7 @@ export default function Sidebar() {
         />
         <ul className="space-y-7">
           <SidebarItems
+            current={pathname === '/dashboard'}
             pathname="/dashboard"
             src="/icons/squares.svg"
             alt="dashboard icon"
@@ -23,6 +34,7 @@ export default function Sidebar() {
           </SidebarItems>
 
           <SidebarItems
+            current={pathname === '/companies'}
             pathname="/companies"
             src="/icons/briefcase.svg"
             alt="companies icon"
@@ -30,7 +42,10 @@ export default function Sidebar() {
             Companies
           </SidebarItems>
         </ul>
-        <button className="flex items-center gap-2 p-6 mx-auto mt-auto">
+        <button
+          className="flex items-center gap-2 p-6 mx-auto mt-auto"
+          onClick={hendleExitClick}
+        >
           <Image
             className=""
             src="/icons/arrow-left-on-rectangle.svg"
